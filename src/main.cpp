0f99fbef2820e35f290e91d6ac6638ec43cc32e4
shikanai327327 <shikanai327327@gmail.com>
2018-02-22 13:49:06 +0900

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,603 @@
+﻿/************************************************************************************
+Filename    :   main.cpp
+Content     :   First-person view test application for Oculus Rift
+Created     :   11th May 2015
+Authors     :   Tom Heath
+Copyright   :   Copyright 2015 Oculus, Inc. All Rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*************************************************************************************/
+/// This is an entry-level sample, showing a minimal VR sample, 
+/// in a simple environment.  Use WASD keys to move around, and cursor keys.
+/// Dismiss the health and safety warning by tapping the headset, 
+/// or pressing any key. 
+/// It runs with DirectX11.
+// Editor : Wizapply : Ovrvision Team
+
+// Include DirectX
+#include "Win32_DirectXAppUtil.h"
+
+#include "OVR_CAPI_0_8_0.h"
+
+// Include the Oculus SDK
+#include "OVR_CAPI_D3D.h"
+
+// OvrvisionPro
+#include <ovrvision_pro.h>
+
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
+extern int InitializeCamPlane(ID3D11Device* Device, ID3D11DeviceContext* DeviceContext, int w, int h, float zsize);
+extern int RendererCamPlane(ID3D11Device* Device, ID3D11DeviceContext* DeviceContext);
+extern int SetCamImage(ID3D11DeviceContext* DeviceContext, unsigned char* camImage, unsigned int imagesize);
+extern int CleanCamPlane();
+//extern void start(float x, float y, float z);
+
+extern int va_canny_arg1;
+extern int va_canny_arg2;
+
+extern bool va_init(unsigned int height, unsigned int width, unsigned int pixelsize);
+extern unsigned char* va_effect(unsigned char* src, unsigned int height, unsigned int width, unsigned int pixelsize, float x, float y, float z);
+extern bool va_close();
+
+float x;
+float y;
+float z;
+
+boolean record_mode = false;
+boolean play_mode = false;
+
+struct ListNode
+{
+	float x;
+	float y;
+	float z;
+	struct ListNode * next;
+};
+ListNode *headL = NULL;
+ListNode *headR = NULL;
+
+//------------------------------------------------------------
+// ovrSwapTextureSet wrapper class that also maintains the render target views
+// needed for D3D11 rendering.
+struct OculusTexture
+{
+    ovrHmd                   hmd;
+	ovrSwapTextureSet      * TextureSet;
+    static const int         TextureCount = 2;
+	ID3D11RenderTargetView * TexRtv[TextureCount];
+
+    OculusTexture() :
+        hmd(nullptr),
+        TextureSet(nullptr)
+    {
+        TexRtv[0] = TexRtv[1] = nullptr;
+    }
+
+    bool Init(ovrHmd _hmd, int sizeW, int sizeH)
+	{
+        hmd = _hmd;
+
+		D3D11_TEXTURE2D_DESC dsDesc;
+		dsDesc.Width = sizeW;
+		dsDesc.Height = sizeH;
+		dsDesc.MipLevels = 1;
+		dsDesc.ArraySize = 1;
+        dsDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+		dsDesc.SampleDesc.Count = 1;   // No multi-sampling allowed
+		dsDesc.SampleDesc.Quality = 0;
+		dsDesc.Usage = D3D11_USAGE_DEFAULT;
+		dsDesc.CPUAccessFlags = 0;
+		dsDesc.MiscFlags = 0;
+		dsDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
+
+		ovrResult result = ovr_CreateSwapTextureSetD3D11(hmd, DIRECTX.Device, &dsDesc, ovrSwapTextureSetD3D11_Typeless, &TextureSet);
+        if (!OVR_SUCCESS(result))
+            return false;
+
+        VALIDATE(TextureSet->TextureCount == TextureCount, "TextureCount mismatch.");
+
+		for (int i = 0; i < TextureCount; ++i)
+		{
+			ovrD3D11Texture* tex = (ovrD3D11Texture*)&TextureSet->Textures[i];
+			D3D11_RENDER_TARGET_VIEW_DESC rtvd = {};
+			rtvd.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+			rtvd.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+			DIRECTX.Device->CreateRenderTargetView(tex->D3D11.pTexture, &rtvd, &TexRtv[i]);
+		}
+
+        return true;
+    }
+
+	~OculusTexture()
+	{
+		for (int i = 0; i < TextureCount; ++i)
+        {
+            Release(TexRtv[i]);
+        }
+		if (TextureSet)
+        {
+            ovr_DestroySwapTextureSet(hmd, TextureSet);
+        }
+	}
+
+	void AdvanceToNextTexture()
+	{
+		TextureSet->CurrentIndex = (TextureSet->CurrentIndex + 1) % TextureSet->TextureCount;
+	}
+};
+
+void appendL(float x, float y, float z)
+{
+	ListNode *newNode;  // 新たなノード
+	ListNode *nodePtr;  // リスト内を移動する用のノード
+
+						// 新たなノードのメモリを割り当て、
+						// valueにnumを代入、
+						// nextにNULLをセット
+	newNode = new ListNode;
+	newNode->x = x;
+	newNode->y = y;
+	newNode->z = z;
+	newNode->next = NULL;
+
+	// リストにノードがなければ、
+	// newNodeを最初のノードにする
+	if (!headL)
+	{
+		headL = newNode;
+	}
+	else  // そうでなければ、最後にnewNodeを挿入する
+	{
+		// nodePtrをリストの先頭として初期化する
+		nodePtr = headL;
+
+		// リストの最後のノード（nodePtr->nextがnull）を見つける
+		// nodePtr->nextがnullであるとき、ループを出る
+		while (nodePtr->next)
+		{
+			nodePtr = nodePtr->next;
+		}
+
+		// 最後のノードとしてnewNodeを挿入する
+		nodePtr->next = newNode;
+	}
+
+}
+
+void appendR(float x, float y, float z)
+{
+	ListNode *newNode;  // 新たなノード
+	ListNode *nodePtr;  // リスト内を移動する用のノード
+
+						// 新たなノードのメモリを割り当て、
+						// valueにnumを代入、
+						// nextにNULLをセット
+	newNode = new ListNode;
+	newNode->x = x;
+	newNode->y = y;
+	newNode->z = z;
+	newNode->next = NULL;
+
+	// リストにノードがなければ、
+	// newNodeを最初のノードにする
+	if (!headR)
+	{
+		headR = newNode;
+	}
+	else  // そうでなければ、最後にnewNodeを挿入する
+	{
+		// nodePtrをリストの先頭として初期化する
+		nodePtr = headR;
+
+		// リストの最後のノード（nodePtr->nextがnull）を見つける
+		// nodePtr->nextがnullであるとき、ループを出る
+		while (nodePtr->next)
+		{
+			nodePtr = nodePtr->next;
+		}
+
+		// 最後のノードとしてnewNodeを挿入する
+		nodePtr->next = newNode;
+	}
+
+}
+
+void pickL()
+{
+	if (!headL)
+	{
+		return;
+	}
+	else
+	{
+		x = headL->x;
+		y = headL->y;
+		z = headL->z;
+		headL = headL->next;
+	}
+}
+
+void pickR()
+{
+	if (!headR)
+	{
+		return;
+	}
+	else
+	{
+		x = headR->x;
+		y = headR->y;
+		z = headR->z;
+		headR = headR->next;
+	}
+}
+
+/*
+void start(ovrHmd hmd)
+{
+	//hmd = OVRManager.capiHmd;
+
+	OVRManager.GetAcceleration(0, ref m_a[0], ref m_a[1], ref m_a[2]);
+}
+*/
+
+// return true to retry later (e.g. after display lost)
+static bool MainLoop(bool retryCreate)
+{
+    // Initialize these to nullptr here to handle device lost failures cleanly
+	ovrTexture     * mirrorTexture = nullptr;
+	OculusTexture  * pEyeRenderTexture[2] = { nullptr, nullptr };
+	DepthBuffer    * pEyeDepthBuffer[2] = { nullptr, nullptr };
+    Camera         * mainCam = nullptr;
+	D3D11_TEXTURE2D_DESC td = {};
+
+	OVR::OvrvisionPro ovrvision;
+	int width = 0, height = 0, pixelsize=4;
+
+	ovrHmd HMD;
+	ovrGraphicsLuid luid;
+	ovrResult result = ovr_Create(&HMD, &luid);
+    if (!OVR_SUCCESS(result))
+        return retryCreate;
+
+    ovrHmdDesc hmdDesc = ovr_GetHmdDesc(HMD);
+	cv::Mat graph1, graph2, graph3;
+
+	// Setup Device and Graphics
+	// Note: the mirror window can be any size, for this sample we use 1/2 the HMD resolution
+    if (!DIRECTX.InitDevice(hmdDesc.Resolution.w / 2, hmdDesc.Resolution.h / 2, reinterpret_cast<LUID*>(&luid)))
+        goto Done;
+
+	// Make the eye render buffers (caution if actual size < requested due to HW limits). 
+	ovrRecti         eyeRenderViewport[2];
+
+	for (int eye = 0; eye < 2; ++eye)
+	{
+		ovrSizei idealSize = ovr_GetFovTextureSize(HMD, (ovrEyeType)eye, hmdDesc.DefaultEyeFov[eye], 1.0f);
+		pEyeRenderTexture[eye] = new OculusTexture();
+		if (!pEyeRenderTexture[eye]->Init(HMD, idealSize.w, idealSize.h))
+		{
+			if (retryCreate) goto Done;
+			VALIDATE(OVR_SUCCESS(result), "Failed to create eye texture.");
+		}
+		pEyeDepthBuffer[eye] = new DepthBuffer(DIRECTX.Device, idealSize.w, idealSize.h);
+		eyeRenderViewport[eye].Pos.x = 0;
+		eyeRenderViewport[eye].Pos.y = 0;
+		eyeRenderViewport[eye].Size = idealSize;
+		if (!pEyeRenderTexture[eye]->TextureSet)
+		{
+			if (retryCreate) goto Done;
+			VALIDATE(false, "Failed to create texture.");
+		}
+	}
+
+	// Create a mirror to see on the monitor.
+	td.ArraySize = 1;
+    td.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	td.Width = DIRECTX.WinSizeW;
+	td.Height = DIRECTX.WinSizeH;
+	td.Usage = D3D11_USAGE_DEFAULT;
+	td.SampleDesc.Count = 1;
+	td.MipLevels = 1;
+    result = ovr_CreateMirrorTextureD3D11(HMD, DIRECTX.Device, &td, 0, &mirrorTexture);
+    if (!OVR_SUCCESS(result))
+    {
+        if (retryCreate) goto Done;
+        VALIDATE(false, "Failed to create mirror texture.");
+    }
+
+	// Create camera
+    mainCam = new Camera(&XMVectorSet(0.0f, 1.6f, 5.0f, 0), &XMQuaternionIdentity());
+
+	// Setup VR components, filling out description
+	ovrEyeRenderDesc eyeRenderDesc[2];
+	eyeRenderDesc[0] = ovr_GetRenderDesc(HMD, ovrEye_Left, hmdDesc.DefaultEyeFov[0]);
+	eyeRenderDesc[1] = ovr_GetRenderDesc(HMD, ovrEye_Right, hmdDesc.DefaultEyeFov[1]);
+
+    bool isVisible = true;
+
+	int locationID = 0;
+	OVR::Camprop cameraMode = OVR::OV_CAMVR_FULL;
+	if (__argc > 2) {
+		printf("Ovrvisin Pro mode changed.");
+		//__argv[0]; ApplicationPath
+		locationID = atoi(__argv[1]);
+		cameraMode = (OVR::Camprop)atoi(__argv[2]);
+	}
+
+	if (ovrvision.Open(locationID, cameraMode)) {
+		width = ovrvision.GetCamWidth();
+		height = ovrvision.GetCamHeight();
+		pixelsize = ovrvision.GetCamPixelsize();
+
+		ovrvision.SetCameraSyncMode(false);
+
+		InitializeCamPlane(DIRECTX.Device, DIRECTX.Context, width, height, 1.0f);
+
+		va_init(width, height, pixelsize);
+	}
+
+	cv::namedWindow("XY", CV_WINDOW_AUTOSIZE);
+	cv::namedWindow("XZ", CV_WINDOW_AUTOSIZE);
+	cv::namedWindow("YZ", CV_WINDOW_AUTOSIZE);
+
+	graph1 = cv::Mat(500, 500, CV_8UC3, cv::Scalar(255, 255, 255));
+	graph2 = cv::Mat(500, 500, CV_8UC3, cv::Scalar(255, 255, 255));
+	graph3 = cv::Mat(500, 500, CV_8UC3, cv::Scalar(255, 255, 255));
+
+	cv::line(graph1, cv::Point(250, 0), cv::Point(250, 500), cv::Scalar(0, 0, 0), 1, 1);
+	cv::line(graph1, cv::Point(0, 250), cv::Point(500, 250), cv::Scalar(0, 0, 0), 1, 1);
+	cv::line(graph2, cv::Point(250, 0), cv::Point(250, 500), cv::Scalar(0, 0, 0), 1, 1);
+	cv::line(graph2, cv::Point(0, 250), cv::Point(500, 250), cv::Scalar(0, 0, 0), 1, 1);
+	cv::line(graph3, cv::Point(250, 0), cv::Point(250, 500), cv::Scalar(0, 0, 0), 1, 1);
+	cv::line(graph3, cv::Point(0, 250), cv::Point(500, 250), cv::Scalar(0, 0, 0), 1, 1);
+
+	cv::imshow("XY", graph1);
+	cv::imshow("XZ", graph2);
+	cv::imshow("YZ", graph3);
+	//cv::waitKey(0);
+
+	// Main loop
+	while (DIRECTX.HandleMessages())
+	{
+		/*
+		XMVECTOR forward = XMVector3Rotate(XMVectorSet(0, 0, -0.05f, 0), mainCam->Rot);
+		XMVECTOR right   = XMVector3Rotate(XMVectorSet(0.05f, 0, 0, 0),  mainCam->Rot);
+		
+		if (DIRECTX.Key['W'] || DIRECTX.Key[VK_UP])	  mainCam->Pos = XMVectorAdd(mainCam->Pos, forward);
+		if (DIRECTX.Key['S'] || DIRECTX.Key[VK_DOWN]) mainCam->Pos = XMVectorSubtract(mainCam->Pos, forward);
+		if (DIRECTX.Key['D'])                         mainCam->Pos = XMVectorAdd(mainCam->Pos, right);
+		if (DIRECTX.Key['A'])                         mainCam->Pos = XMVectorSubtract(mainCam->Pos, right);
+		static float Yaw = 0;
+		if (DIRECTX.Key[VK_LEFT])  mainCam->Rot = XMQuaternionRotationRollPitchYaw(0, Yaw += 0.02f, 0);
+		if (DIRECTX.Key[VK_RIGHT]) mainCam->Rot = XMQuaternionRotationRollPitchYaw(0, Yaw -= 0.02f, 0);
+		*/
+
+		/*
+		if (DIRECTX.Key['1']) {
+			va_canny_arg1 = 10; va_canny_arg2 = 20;
+		}
+		if (DIRECTX.Key['2']) {
+			va_canny_arg1 = 20; va_canny_arg2 = 40;
+		}
+		if (DIRECTX.Key['3']) {
+			va_canny_arg1 = 40; va_canny_arg2 = 80;
+		}
+		if (DIRECTX.Key['4']) {
+			va_canny_arg1 = 80; va_canny_arg2 = 160;
+		}
+		if (DIRECTX.Key['5']) {
+			va_canny_arg1 = 160; va_canny_arg2 = 320;
+		}
+		
+
+		if (DIRECTX.Key['1']) {
+			ovrTrackingState ts = ovr_GetTrackingState(m_OVRSessionId, ovr_GetTimeInSeconds(), ovrTrue);
+			ovr_CalcEyePoses(ts.HeadPose.ThePose, m_hmdToEyeViewOffset, m_OVRLayer.RenderPose);
+			std::cout << 
+		}
+		*/
+
+		if (DIRECTX.Key['1']) {
+			record_mode = true;
+		}
+		if (DIRECTX.Key['2']) {
+			record_mode = false;
+		}
+		if (DIRECTX.Key['3']) {
+			play_mode = true;
+		}
+
+		// Get both eye poses simultaneously, with IPD offset already included. 
+		ovrPosef         EyeRenderPose[2];
+		ovrVector3f      HmdToEyeViewOffset[2] = { eyeRenderDesc[0].HmdToEyeViewOffset,
+			                                       eyeRenderDesc[1].HmdToEyeViewOffset };
+        double frameTime = ovr_GetPredictedDisplayTime(HMD, 0);
+        // Keeping sensorSampleTime as close to ovr_GetTrackingState as possible - fed into the layer
+        double           sensorSampleTime = ovr_GetTimeInSeconds();
+		ovrTrackingState hmdState = ovr_GetTrackingState(HMD, frameTime, ovrTrue);
+		ovr_CalcEyePoses(hmdState.HeadPose.ThePose, HmdToEyeViewOffset, EyeRenderPose);
+		
+		ovrvision.PreStoreCamData(OVR::Camqt::OV_CAMQT_DMSRMP);
+
+		/*
+		if (record_mode) {
+			appendL(EyeRenderPose[0].Position.x, EyeRenderPose[0].Position.y, EyeRenderPose[0].Position.z);
+			appendR(EyeRenderPose[1].Position.x, EyeRenderPose[1].Position.y, EyeRenderPose[1].Position.z);
+		}
+
+		if (play_mode) {
+			while (headL != NULL) {
+				pickL();
+				cv::circle(graph1, cv::Point(x * 200 + 250, -y * 200 + 250), 1, cv::Scalar(255, 0, 0), -1, CV_AA, 0);
+				cv::imshow("XY", graph1);
+				cv::circle(graph2, cv::Point(x * 200 + 250, -z * 200 + 250), 1, cv::Scalar(0, 255, 0), -1, CV_AA, 0);
+				cv::imshow("XZ", graph2);
+				cv::circle(graph3, cv::Point(-y * 200 + 250, -z * 200 + 250), 1, cv::Scalar(0, 0, 255), -1, CV_AA, 0);
+				cv::imshow("YZ", graph3);
+				Sleep(5 * 100);
+			}
+			play_mode = false;
+		}
+		*/
+
+		
+		appendL(EyeRenderPose[0].Position.x, EyeRenderPose[0].Position.y, EyeRenderPose[0].Position.z);
+		appendR(EyeRenderPose[1].Position.x, EyeRenderPose[1].Position.y, EyeRenderPose[1].Position.z);
+
+		cv::circle(graph1, cv::Point(EyeRenderPose[0].Position.x * 200 + 250, - EyeRenderPose[0].Position.y * 200 + 250), 1, cv::Scalar(255, 0, 0), -1, CV_AA, 0);
+		cv::imshow("XY", graph1);
+		cv::circle(graph2, cv::Point(EyeRenderPose[0].Position.x * 200 + 250, - EyeRenderPose[0].Position.z * 200 + 250), 1, cv::Scalar(0, 255, 0), -1, CV_AA, 0);
+		cv::imshow("XZ", graph2);
+		cv::circle(graph3, cv::Point(- EyeRenderPose[0].Position.y * 200 + 250, - EyeRenderPose[0].Position.z * 200 + 250), 1, cv::Scalar(0, 0, 255), -1, CV_AA, 0);
+		cv::imshow("YZ", graph3);
+		
+
+		/*
+		if (DIRECTX.Key['1']) {
+			float x = HmdToEyeViewOffset[0].x;
+			float y = HmdToEyeViewOffset[0].y;
+			float z = HmdToEyeViewOffset[0].z;
+			start(x, y, z);
+		}
+		*/
+
+		// Render Scene to Eye Buffers
+        if (isVisible)
+        {
+            for (int eye = 0; eye < 2; ++eye)
+		    {
+			    // Increment to use next texture, just before writing
+			    pEyeRenderTexture[eye]->AdvanceToNextTexture();
+
+			    // Clear and set up rendertarget
+			    int texIndex = pEyeRenderTexture[eye]->TextureSet->CurrentIndex;
+			    DIRECTX.SetAndClearRenderTarget(pEyeRenderTexture[eye]->TexRtv[texIndex], pEyeDepthBuffer[eye]);
+			    DIRECTX.SetViewport((float)eyeRenderViewport[eye].Pos.x, (float)eyeRenderViewport[eye].Pos.y,
+				    (float)eyeRenderViewport[eye].Size.w, (float)eyeRenderViewport[eye].Size.h);
+
+			    //Get the pose information in XM format
+			    XMVECTOR eyeQuat = XMVectorSet(EyeRenderPose[eye].Orientation.x, EyeRenderPose[eye].Orientation.y,
+				                               EyeRenderPose[eye].Orientation.z, EyeRenderPose[eye].Orientation.w);
+			    XMVECTOR eyePos = XMVectorSet(EyeRenderPose[eye].Position.x, EyeRenderPose[eye].Position.y, EyeRenderPose[eye].Position.z, 0);
+
+			    // Get view and projection matrices for the Rift camera
+			    XMVECTOR CombinedPos = XMVectorAdd(mainCam->Pos, XMVector3Rotate(eyePos, mainCam->Rot));
+			    Camera finalCam(&CombinedPos, &(XMQuaternionMultiply(eyeQuat,mainCam->Rot)));
+			    XMMATRIX view = finalCam.GetViewMatrix();
+			    ovrMatrix4f p = ovrMatrix4f_Projection(eyeRenderDesc[eye].Fov, 0.2f, 1000.0f, ovrProjection_RightHanded);
+			    XMMATRIX proj = XMMatrixSet(p.M[0][0], p.M[1][0], p.M[2][0], p.M[3][0],
+				                            p.M[0][1], p.M[1][1], p.M[2][1], p.M[3][1],
+				                            p.M[0][2], p.M[1][2], p.M[2][2], p.M[3][2],
+				                            p.M[0][3], p.M[1][3], p.M[2][3], p.M[3][3]);
+			    XMMATRIX prod = XMMatrixMultiply(view, proj);
+
+				//Camera View
+				/*
+				if (eye == 0)
+					SetCamImage(DIRECTX.Context, ovrvision.GetCamImageBGRA(OVR::Cameye::OV_CAMEYE_LEFT), width*pixelsize);
+				else
+					SetCamImage(DIRECTX.Context, ovrvision.GetCamImageBGRA(OVR::Cameye::OV_CAMEYE_RIGHT), width*pixelsize);
+*/
+
+				
+				if (eye == 0) {
+					//SetCamImage(DIRECTX.Context, ovrvision.GetCamImageBGRA(OVR::Cameye::OV_CAMEYE_LEFT), width*pixelsize);
+					SetCamImage(DIRECTX.Context,
+						va_effect(ovrvision.GetCamImageBGRA(OVR::Cameye::OV_CAMEYE_LEFT), width, height, pixelsize, EyeRenderPose[0].Position.x, EyeRenderPose[0].Position.y, EyeRenderPose[0].Position.z),
+						width*pixelsize);
+					/*SetCamImage(DIRECTX.Context,
+						ovrvision.GetCamImageBGRA(OVR::Cameye::OV_CAMEYE_LEFT),
+						width*pixelsize);*/
+				}
+				else
+				{
+					//SetCamImage(DIRECTX.Context, ovrvision.GetCamImageBGRA(OVR::Cameye::OV_CAMEYE_RIGHT), width*pixelsize);
+					/*SetCamImage(DIRECTX.Context,
+						ovrvision.GetCamImageBGRA(OVR::Cameye::OV_CAMEYE_RIGHT),
+						width*pixelsize);*/
+					SetCamImage(DIRECTX.Context,
+						va_effect(ovrvision.GetCamImageBGRA(OVR::Cameye::OV_CAMEYE_RIGHT), width, height, pixelsize, EyeRenderPose[1].Position.x, EyeRenderPose[1].Position.y, EyeRenderPose[1].Position.z),
+						width*pixelsize);
+				}
+				
+				RendererCamPlane(DIRECTX.Device, DIRECTX.Context);
+		    }
+        }
+
+		// Initialize our single full screen Fov layer.
+        ovrLayerEyeFov ld = {};
+		ld.Header.Type = ovrLayerType_EyeFov;
+		ld.Header.Flags = 0;
+
+		for (int eye = 0; eye < 2; ++eye)
+		{
+			ld.ColorTexture[eye] = pEyeRenderTexture[eye]->TextureSet;
+			ld.Viewport[eye] = eyeRenderViewport[eye];
+			ld.Fov[eye] = hmdDesc.DefaultEyeFov[eye];
+			ld.RenderPose[eye] = EyeRenderPose[eye];
+            ld.SensorSampleTime = sensorSampleTime;
+		}
+
+        ovrLayerHeader* layers = &ld.Header;
+        result = ovr_SubmitFrame(HMD, 0, nullptr, &layers, 1);
+        // exit the rendering loop if submit returns an error, will retry on ovrError_DisplayLost
+        if (!OVR_SUCCESS(result))
+            goto Done;
+
+        isVisible = (result == ovrSuccess);
+
+        // Render mirror
+        ovrD3D11Texture* tex = (ovrD3D11Texture*)mirrorTexture;
+        DIRECTX.Context->CopyResource(DIRECTX.BackBuffer, tex->D3D11.pTexture);
+        DIRECTX.SwapChain->Present(0, 0);
+	}
+
+	// Release resources
+Done:
+    delete mainCam;
+	if (mirrorTexture) ovr_DestroyMirrorTexture(HMD, mirrorTexture);
+    for (int eye = 0; eye < 2; ++eye)
+    {
+	    delete pEyeRenderTexture[eye];
+        delete pEyeDepthBuffer[eye];
+    }
+	DIRECTX.ReleaseDevice();
+	ovr_Destroy(HMD);
+
+	ovrvision.Close();
+	CleanCamPlane();
+
+	va_close();
+
+    // Retry on ovrError_DisplayLost
+    return retryCreate || OVR_SUCCESS(result) || (result == ovrError_DisplayLost);
+}
+
+//-------------------------------------------------------------------------------------
+int WINAPI WinMain(HINSTANCE hinst, HINSTANCE, LPSTR, int)
+{
+	// Initializes LibOVR, and the Rift
+	ovrResult result = ovr_Initialize(nullptr);
+	VALIDATE(OVR_SUCCESS(result), "Failed to initialize libOVR.");
+
+    VALIDATE(DIRECTX.InitWindow(hinst, L"Ovrvision Pro for OculusSDK"), "Failed to open window.");
+
+    DIRECTX.Run(MainLoop);
+
+	ovr_Shutdown();
+	return(0);
+}
---
+++ src/vision_augmentation.cpp
@@ -0,0 +1,78 @@
+﻿// vision_augmentation.cpp
+// simple image processing for vision augmentation
+
+// OpenCV
+#include <opencv2/opencv.hpp>
+//using namespace cv;
+
+bool _va_init = false;
+unsigned int _va_width, _va_height, _va_pixelsize;
+cv::Mat cv_src, cv_grey, cv_dst;
+int va_canny_arg1 = 50;
+int va_canny_arg2 = 200;
+
+bool va_init(unsigned int width, unsigned int height, unsigned int pixelsize) {
+
+	if (_va_init) return false;
+
+	if (pixelsize != 4) return false;
+	_va_width = width, _va_height = height, _va_pixelsize = pixelsize;
+
+	cv_src = cv::Mat(_va_height, _va_width, CV_8UC4);
+	cv_grey = cv::Mat(_va_height, _va_width, CV_8UC1);
+	cv_dst = cv::Mat(_va_height, _va_width, CV_8UC4);
+	_va_init = true;
+	return true;
+}
+
+/*
+void start(float x, float y, float z)
+{
+	std::stringstream ss;
+	ss << x << ", " << y << ", " << z;
+
+	//cv::putText(cv_src, ss.str() , cv::Point2f(50, .0f),
+	//	cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(0, 255, 0), 4, CV_AA);
+	cv::putText(cv_src, "Test" , cv::Point2f(50, .0f),
+		cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(0, 255, 0), 4, CV_AA);
+}
+*/
+
+unsigned char* va_effect(unsigned char* src, unsigned int width, unsigned int height, unsigned int pixelsize, float x, float y, float z) {
+	cv::Mat cv_rgb;
+	if (!_va_init) return NULL;
+	if (_va_width != width || _va_height != height || _va_pixelsize != pixelsize) return NULL;
+
+	std::stringstream ss;
+	ss << x << ", " << y << ", " << z;
+
+	memcpy(cv_src.data, src, _va_width * _va_height * _va_pixelsize);
+	cv::cvtColor(cv_src, cv_rgb, cv::COLOR_BGRA2BGR);
+	//cv::imshow("rgb", cv_rgb);
+	//cv::waitKey(1);
+
+	cv::putText(cv_rgb, ss.str(), cv::Point2f(50, 500),
+	cv::FONT_HERSHEY_SIMPLEX, 1.5, cv::Scalar(0, 255, 0), 4, CV_AA);
+	cv::cvtColor(cv_rgb, cv_dst, cv::COLOR_BGR2BGRA);
+	return cv_dst.data;
+}
+
+/*
+unsigned char* va_effect(unsigned char* src, unsigned int width, unsigned int height, unsigned int pixelsize) {
+	if (!_va_init) return NULL;
+	if (_va_width != width || _va_height != height || _va_pixelsize != pixelsize) return NULL;
+
+	memcpy(cv_src.data, src, _va_width * _va_height * _va_pixelsize);
+	//cv::Canny(cv_src, cv_grey, va_canny_arg1, va_canny_arg2);
+	//cv::cvtColor(cv_grey, cv_dst, CV_GRAY2BGRA);
+	cv::cvtColor(cv_src, cv_dst, CV_RGB2BGR);
+	return cv_dst.data;
+}
+*/
+
+bool va_close() {
+	if (!_va_init) return false;
+
+	_va_init = false;
+	return true;
+}
